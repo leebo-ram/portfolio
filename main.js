1f68a3e1dfaa936439f5d999dcffb1e70a158b2f
leebo-ram <lbr71004@gmail.com>
2022-12-07 00:27:17 +0900

    CSS: Activate the menu for that section when scrolling

--- main.js
+++ main.js
@@ -69,15 +69,16 @@ workBtnContainer.addEventListener('click', (e) => {
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
 
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
       console.log(project.dataset.type);
-      if(filter === '*' || filter === project.dataset.type) {
+      if (filter === '*' || filter === project.dataset.type) {
         project.classList.remove('invisible');
       } else {
         project.classList.add('invisible');
@@ -88,30 +89,69 @@ workBtnContainer.addEventListener('click', (e) => {
 });
 
 
+
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)])
 }
 
-  
-//-------------------------아래는 내가 힘으로 해결한 것------------------------------------
-// const contact_btn = document.querySelector('.home__contact');
-// contact_btn.addEventListener('click', (event) => {
-//   console.log(event.target.dataset.link);
-//   const target = event.target;
-//   const link = target.dataset.link;
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
 
-//   const scrollTo = document.querySelector(link);
-//   scrollTo.scrollIntoView({behavior: "smooth"});
-// });
-// --------------------------------------------------------------------------------------
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      console.log('y');
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
 
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
 
-// // --------------------------아래는 내가 응용해서 해결 한 것-------------------------------
-// const toggleBtn = document.querySelector('.navbar__toggle-btn');
-// const menu = document.querySelector('.navbar__menu');
-
-// toggleBtn.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-// });
-// --------------------------------------------------------------------------------------
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
